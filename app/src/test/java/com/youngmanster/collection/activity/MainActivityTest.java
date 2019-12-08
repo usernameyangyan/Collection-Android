c49a30268584c237b045fe7f4d3233d61b88bd28
usernameyangyan <18475159376@163.com>
2019-12-08 21:32:09 +0800

    编译修改

--- app/src/test/java/com/youngmanster/collection/activity/MainActivityTest.java
+++ app/src/test/java/com/youngmanster/collection/activity/MainActivityTest.java
@@ -1,39 +1,33 @@
-package com.youngmanster.collection.ui;
-
-import android.widget.TextView;
-
-import com.youngmanster.collection.activity.MainActivity;
+package com.youngmanster.collection.activity;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * Created by yangy
- * 2019-12-07
+ * 2019-12-08
  * Describe:
  */
 @RunWith(RobolectricTestRunner.class)
+@Config(sdk = 25)
 public class MainActivityTest {
     private MainActivity mainActivity;
 
-
     @Before
-    public void setUp(){
+    public void setUp() {
         mainActivity= Robolectric.buildActivity(MainActivity.class)
                 .create()
                 .resume()
                 .get();
-
     }
-
     @Test
-    public void onClickSubmitBtn(){
-        assertNull(mainActivity);
+    public void onCLick(){
+        assertNotNull(mainActivity);
     }
 }
